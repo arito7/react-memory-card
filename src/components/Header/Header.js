86f8c9ba1ae44c93d765b73c9482799bb7ea3a75
Yuji T <accmanager1990@gmail.com>
2022-02-13 02:53:01 +0900

    Add header with styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <h1 className="title">Memory Card</h1>
+    </div>
+  );
+};
+
+export default Header;
