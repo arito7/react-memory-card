94159fa32cc90b18c222affb42f1fae8d37dbf1b
Yuji T <accmanager1990@gmail.com>
2022-02-13 03:07:05 +0900

    Add styling using CSS

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,39 +1,16 @@
 import React from 'react';
 import GithubIcon from './GitHub-Mark-Light-32px.png';
-import styled from 'styled-components';
+import './Footer.css';
+
 const Footer = () => {
   return (
-    <FooterWrapper className="footer">
+    <div className="footer">
       <span>Copyright &#169; 2022 arito7</span>
-      <Anchor href="https://github.com/arito7/arito7">
-        <FooterImg src={GithubIcon}></FooterImg>
-      </Anchor>
-    </FooterWrapper>
+      <a href="https://github.com/arito7/arito7">
+        <img src={GithubIcon} alt="github icon" />
+      </a>
+    </div>
   );
 };
-
-const FooterWrapper = styled.div`
-  color: white;
-  display: grid;
-  align-items: center;
-  margin-top: 1rem;
-  display: grid;
-  width: 100%;
-  background-color: rgb(51, 46, 46);
-  grid-template-rows: 1fr;
-  grid-template-columns: repeat(2, auto);
-  justify-content: center;
-  padding: 0.25rem 0;
-`;
-
-const Anchor = styled.a`
-  display: grid;
-  align-items: center;
-`;
-const FooterImg = styled.img`
-  margin-left: 0.5rem;
-  width: 1.5rem;
-  height: 1.5rem;
-`;
 
 export default Footer;
