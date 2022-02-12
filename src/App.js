385050126b67e4d7b6aaf34f128e4af621680d11
Yuji T <accmanager1990@gmail.com>
2022-02-13 02:55:03 +0900

    Add logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CardsContainer from './components/Card/CardsContainer';
+import { items as uItems, shuffle } from './Util';
+import { useEffect, useState } from 'react';
+import Score from './components/Score/Score';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
+  const [score, setScore] = useState({ score: 0, currentHighest: 0 });
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [items, setItems] = useState(uItems);
+
+  const onCardClick = (item) => {
+    setSelectedItems((prevState) => {
+      if (!prevState.includes(item.name)) {
+        setScore({ ...score, score: score.score + 1 });
+        setItems(shuffle(items));
+        return selectedItems.concat(item.name);
+      }
+      setScore((prevState) => {
+        if (prevState.currentHighest > score.score) {
+          return { ...score, score: 0 };
+        }
+        return { currentHighest: score.score, score: 0 };
+      });
+      setItems(shuffle(items));
+      return [];
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Score score={score} />
+      <CardsContainer items={items} cardClickHandler={onCardClick} />
+      <Footer />
     </div>
   );
 }
